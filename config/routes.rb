bf578da055eb30694d7102dd9fd10769a505ad43
EC2 Default User <ec2-user@ip-172-31-16-193.ap-northeast-1.compute.internal>
2023-01-22 17:01:12 +0000

    [Add/Finish] アプリケーションに必要なファイルの準備 / ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/books' => 'books#index'
+  post 'books' => 'books#create'
+  root to: 'homes#top'
 end
