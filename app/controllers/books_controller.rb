b80bb812d57cb644e33996115127450fb119448a
EC2 Default User <ec2-user@ip-172-31-16-193.ap-northeast-1.compute.internal>
2023-02-04 12:47:16 +0000

    [Add/Fix] topページのstartリンク修正、index,show,editページの作成と各機能の不具合修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,28 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    @book.save
+    flash[:notice] = "Book was successfully crated."
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
 
 private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 
 end
