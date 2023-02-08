cf9d38bdac4f23f40469d05d4b5729d141b6bcb5
EC2 Default User <ec2-user@ip-172-31-16-193.ap-northeast-1.compute.internal>
2023-02-08 13:47:55 +0000

    [Add/Fix] create,update機能設定 / フラッシュメッセージ,バリデーションの実装 / 機能の不具合修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    flash[:notice] = "Book was successfully crated."
-    redirect_to book_path(book.id)
-  end
-
-  def index
-    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully crated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,10 +23,26 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully crated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
 
 private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
